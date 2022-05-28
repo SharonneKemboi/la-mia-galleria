cfd4691b8c6e2e304f4b54074986664036ada3c6
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-05-28 16:29:54 +0300

    Register Models in Admin

--- migalleria/admin.py
+++ migalleria/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Image,Place,Category
+
+admin.site.register(Image)
+admin.site.register(Place)
+admin.site.register(Category)
