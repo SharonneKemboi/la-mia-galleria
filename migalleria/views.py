a3096c5a2c5c5943c3ce9fc431b820aa049caa2b
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-05-28 14:50:04 +0300

    Register App and create Views

--- migalleria/views.py
+++ migalleria/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse,Http404
+
 # Create your views here.
+def index(request):
+    return HttpResponse("Welcome To My Gallery") 
+
