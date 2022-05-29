4b762b4c64c37e85780acfe78c0955fc68ca9af1
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-05-29 18:23:23 +0300

    Add a search and places_page function

--- migalleria/views.py
+++ migalleria/views.py
@@ -1,15 +1,33 @@
+
 from django.shortcuts import render
 from .models import Image,Place,Category
 from django.http import HttpResponse,Http404
 
 # Create your views here.
 def index(request):
-    images = Image.get_images()
+    gallery = Image.get_images()
 
 
-    three_images_list = [images[x:x+3] for x in range(0, len(images),3)]
-
-
-    context = {"images":images}
+    context = {"images":gallery}
 
     return render(request,"index.html",context) 
+
+def search(request):
+    if "term" in request.GET and request.GET["term"]:
+
+        term = request.GET.get("term")
+        gallery = Image.search_image(term)
+        if gallery != "No images found":
+            message = "Gallery of " + term + ""
+
+            return render(request, "search.html", {"images":gallery,"message":message,"title":term.capitalize()})
+        else:
+            message = "No images found"
+            return render(request, "search.html", {"images":[],"message":message,"title":term.capitalize()})
+
+
+def places_page(request):
+    return render(request,"places.html",{"title":"Places"})
+
+
+    
